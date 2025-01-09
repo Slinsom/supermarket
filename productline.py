b2693301fa634dd26e526916d9fd48fc3023e1af
Slinsom <rifqialfakhir29@gmail.com>
2025-01-09 22:33:28 +0800

    Update productline.py

--- productline.py
+++ productline.py
@@ -27,17 +27,6 @@ def get_genders_by_product_line(product_line):
     genders = df[df['Product line'] == product_line]['Gender'].value_counts()
     return genders
 
-# Fungsi untuk menghitung rata-rata pembelian
-def get_average_purchases(product_line):
-    total_purchases = df[df['Product line'] == product_line].shape[0]
-    return total_purchases
-
-# Fungsi untuk menghitung rata-rata rating
-def get_average_rating(product_line):
-    ratings = df[df['Product line'] == product_line]['Rating']
-    avg_rating = ratings.mean()
-    return avg_rating
-
 # Fungsi untuk menghitung rata-rata unit price
 def get_average_unit_price(product_line):
     unit_price = df[df['Product line'] == product_line]['Unit price']
@@ -49,6 +38,17 @@ def get_average_quantity(product_line):
     quantity = df[df['Product line'] == product_line]['Quantity']
     avg_quantity = quantity.mean()
     return avg_quantity
+
+# Fungsi untuk menghitung rata-rata rating
+def get_average_rating(product_line):
+    ratings = df[df['Product line'] == product_line]['Rating']
+    avg_rating = ratings.mean()
+    return avg_rating
+
+# Fungsi untuk menghitung rata-rata pembelian
+def get_average_purchases(product_line):
+    total_purchases = df[df['Product line'] == product_line].shape[0]
+    return total_purchases
 
 # Antarmuka Streamlit
 st.title('Aplikasi Analisis Product Line dan Pembelian')
@@ -72,14 +72,6 @@ if product_line:
     st.write(f'Pembeli berdasarkan gender untuk product line "{product_line}":')
     st.write(genders)
 
-    # Menampilkan rata-rata pembelian
-    avg_purchases = get_average_purchases(product_line)
-    st.write(f'Rata-rata jumlah pembelian untuk product line "{product_line}": {avg_purchases}')
-
-    # Menampilkan rata-rata rating
-    avg_rating = get_average_rating(product_line)
-    st.write(f'Rata-rata rating untuk product line "{product_line}": {avg_rating:.2f}')
-
     # Menampilkan rata-rata unit price
     avg_unit_price = get_average_unit_price(product_line)
     st.write(f'Rata-rata unit price untuk product line "{product_line}": ${avg_unit_price:.2f}')
@@ -87,3 +79,11 @@ if product_line:
     # Menampilkan rata-rata quantity
     avg_quantity = get_average_quantity(product_line)
     st.write(f'Rata-rata quantity untuk product line "{product_line}": {avg_quantity:.2f}')
+    
+    # Menampilkan rata-rata rating
+    avg_rating = get_average_rating(product_line)
+    st.write(f'Rata-rata rating untuk product line "{product_line}": {avg_rating:.2f}')
+
+    # Menampilkan rata-rata pembelian
+    avg_purchases = get_average_purchases(product_line)
+    st.write(f'Rata-rata jumlah pembelian untuk product line "{product_line}": {avg_purchases}')

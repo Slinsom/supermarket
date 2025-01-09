5541d4869a453d9fbd2d2ac4f5b83941d76a911d
Slinsom <rifqialfakhir29@gmail.com>
2025-01-09 21:06:03 +0800

    Update productline.py

--- productline.py
+++ productline.py
@@ -5,7 +5,7 @@ from sklearn.ensemble import RandomForestClassifier
 import joblib
 
 # Load dataset
-data = pd.read_csv('Supermarket Sales Cleaned.csv')
+data = pd.read_csv('supermarket.csv')
 
 # Preprocessing
 data = pd.get_dummies(data, columns=['Gender', 'Payment'])
